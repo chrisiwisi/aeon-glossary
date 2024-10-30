7e3fb752f7cb1adecf59d7f129aa83d39cac283d
Christian Wieser <e1642997@student.tuwien.ac.at>
2024-10-30 16:15:43 +0100

    Added AIonGlossary Spring Backend initial commit

---
+++ AionGlossary/src/main/java/TodlicherTeddy/AionGlossary/AionGlossaryApplication.java
@@ -0,0 +1,13 @@
+package TodlicherTeddy.AionGlossary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AionGlossaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AionGlossaryApplication.class, args);
+	}
+
+}
---
+++ AionGlossary/src/test/java/TodlicherTeddy/AionGlossary/AionGlossaryApplicationTests.java
@@ -0,0 +1,13 @@
+package TodlicherTeddy.AionGlossary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AionGlossaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
