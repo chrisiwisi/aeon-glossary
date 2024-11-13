c5801c807da5074fdb228457c38825ae159d59a4
Christian Wieser <e1642997@student.tuwien.ac.at>
2024-11-13 17:19:34 +0100

    added prompt and await full thread method

--- Client/src/app/aion-chatbot/aion.service.ts
+++ Client/src/app/aion-chatbot/aion.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AionService {
+  private baseUri: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  promptAndAwaitFullThread(prompt: string): Observable<any> {
+    const options = {
+      params: new HttpParams().set('message', prompt)
+    };
+    return this.http.get<any>(this.baseUri + '/messages', options);
+  }
 }
