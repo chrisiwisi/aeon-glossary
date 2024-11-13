f46bb39f140900e27d53d0cc431082eb1a7fce43
Christian Wieser <e1642997@student.tuwien.ac.at>
2024-11-13 17:06:32 +0100

    added aion chatbot components

---
+++ Client/src/app/aion-chatbot/chat-prompt/chat-prompt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-prompt',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-prompt.component.html',
+  styleUrl: './chat-prompt.component.css'
+})
+export class ChatPromptComponent {
+
+}
