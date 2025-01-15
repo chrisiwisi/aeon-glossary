506249a5e4f37eb24499b70441665ee48c07157c
Christian Wieser <e1642997@student.tuwien.ac.at>
2025-01-15 16:17:59 +0100

    added input fields to change the values of letters

--- Client/src/app/code-note/code-note.component.ts
+++ Client/src/app/code-note/code-note.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {KeyValuePipe, NgForOf} from "@angular/common";
 import {DecodePipe} from "./decode.pipe";
-import {INVALID_LETTER, Letter} from "./Letter";
+import {INVALID_LETTER, Letter, SPACE_LETTER} from "./Letter";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-code-note',
@@ -9,7 +10,8 @@ import {INVALID_LETTER, Letter} from "./Letter";
   imports: [
     NgForOf,
     DecodePipe,
-    KeyValuePipe
+    KeyValuePipe,
+    FormsModule
   ],
   templateUrl: './code-note.component.html',
   styleUrl: './code-note.component.css'
@@ -20,8 +22,9 @@ export class CodeNoteComponent implements OnInit {
 
   ngOnInit(): void {
     let placeholderLetters = " 123456789アイウエオカキクケコサシスセソタチツテト";
-    for (let i = 0; i < 17; i++) {
-      this.alphabet.set(i, { id: i, romanLetter: "" });
+    this.alphabet.set(0, SPACE_LETTER);
+    for (let i = 1; i < 17; i++) {
+      this.alphabet.set(i, {id: i, romanLetter: ""});
     }
 
     this.alphabet.set(1, {id: 1, romanLetter: 's'});
@@ -29,13 +32,25 @@ export class CodeNoteComponent implements OnInit {
     this.alphabet.set(3, {id: 3, romanLetter: 'v'});
     this.alphabet.set(4, {id: 4, romanLetter: 'r'});
 
-    this.messages.push([1,2,3,2,4,2,0,5,6,7,7,0,8,6,9,1,0,10,11,12].map((value) => {
+    this.messages.push([1, 2, 3, 2, 4, 2, 0, 5, 6, 7, 7, 0, 8, 6, 9, 1, 0, 10, 11, 12].map((value) => {
       let letter = this.alphabet.get(value);
-      return letter? letter : INVALID_LETTER;
+      return letter ? letter : INVALID_LETTER;
     }));
-    this.messages.push([10,11,12,0,3,2,13,14,0,13,6,11,7,2,15,14,1,0,15,3,14,0,7,2,5,6,0,11,8,0,15,6,16,14].map((value) => {
+    this.messages.push([10, 11, 12, 0, 3, 2, 13, 14, 0, 13, 6, 11, 7, 2, 15, 14, 1, 0, 15, 3, 14, 0, 7, 2, 5, 6, 0, 11, 8, 0, 15, 6, 16, 14].map((value) => {
       let letter = this.alphabet.get(value);
-      return letter? letter : INVALID_LETTER;
+      return letter ? letter : INVALID_LETTER;
     }));
+  }
+
+  updateLetter(letterKey: number, event: Event) {
+    const inputElement: HTMLInputElement = event.target as HTMLInputElement;
+    console.log(inputElement);
+    let newValue = inputElement.value;
+    let oldLetter: (Letter | undefined) = this.alphabet.get(letterKey);
+    if (oldLetter) {
+      this.alphabet.set(letterKey, {...oldLetter, romanLetter: newValue});
+      return;
+    }
+    this.alphabet.set(letterKey, {id: letterKey, romanLetter: newValue} as Letter);
   }
 }
