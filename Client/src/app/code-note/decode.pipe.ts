331cc78dde5949c2b085ea787a9d7d3fe839ba68
Christian Wieser <e1642997@student.tuwien.ac.at>
2025-01-19 01:47:10 +0100

    letters are now displayed using their image or roman numerical

--- Client/src/app/code-note/decode.pipe.ts
+++ Client/src/app/code-note/decode.pipe.ts
@@ -8,9 +8,18 @@ import {INVALID_LETTER, Letter} from "./Letter";
 })
 export class DecodePipe implements PipeTransform {
 
-  transform(messageKeys: number[], alphabet: Map<number, Letter>): string {
+  transform(messageKeys: number[], alphabet: Map<number, Letter>): { letter?: string, URL?: string }[] {
     const letterArray: (Letter | undefined)[] = messageKeys.map((value) => alphabet.get(value));
-    return letterArray.map((value: Letter | undefined) => value ? value.romanLetter : INVALID_LETTER.romanLetter).join('');
+    return letterArray.map((letter: Letter | undefined) => {
+      if (letter) {
+        let result = {letter: ''} as { letter?: string, URL?: string };
+        if (letter.imageURL) {result = {URL: letter.imageURL};}
+        if (letter.romanLetter) {result = {...result, letter: letter.romanLetter};}
+        return result;
+      } else {
+        return {letter: INVALID_LETTER.romanLetter};
+      }
+    });
   }
 
 }
