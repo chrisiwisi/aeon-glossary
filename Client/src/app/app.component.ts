e9f0146ffd3af75eca6ce3f46f0feecd7ac6ce79
Christian Wieser <117042014+Christian-Wieser@users.noreply.github.com>
2025-06-12 16:55:10 +0200

    chore: removed deprecated HttpClient

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,19 +1,15 @@
 import {Component, inject} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {RulesCardComponent} from "./rules-card/rules-card.component";
-import { NgClass } from "@angular/common";
 import {SearchPipe} from "./search/search.pipe";
 import {FormsModule} from "@angular/forms";
 import {SearchService} from "./search/search.service";
 import {Rule} from "./rules-card/Rule";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {ConversationComponent} from "./aion-chatbot/conversation/conversation.component";
-import {ChatPromptComponent} from "./aion-chatbot/chat-prompt/chat-prompt.component";
+import {HttpClient} from "@angular/common/http";
 import {AionComponent} from "./aion-chatbot/aion.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RulesCardComponent, SearchPipe, FormsModule, HttpClientModule, AionComponent],
+    imports: [RulesCardComponent, SearchPipe, FormsModule, AionComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
