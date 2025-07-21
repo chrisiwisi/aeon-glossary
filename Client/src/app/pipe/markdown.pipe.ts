637585dfd6f0e3f415c98330dcf1428e182a4fc7
Christian Wieser <117042014+Christian-Wieser@users.noreply.github.com>
2025-07-21 15:13:17 +0200

    refactor: remove unused imports

--- Client/src/app/pipe/markdown.pipe.ts
+++ Client/src/app/pipe/markdown.pipe.ts
@@ -1,12 +1,10 @@
-import {inject, Pipe, PipeTransform} from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {marked} from 'marked';
-import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Pipe({
   name: 'markdown'
 })
 export class MarkdownPipe implements PipeTransform {
-  private sanitizer = inject(DomSanitizer);
 
   transform(value: string |undefined): string | Promise<string> {
     if (!value) {
