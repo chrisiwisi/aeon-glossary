102bf6778607db39ee606aed9cfaf1d9dae2e611
chrisiwisi <wisi.chrisi@gmail.com>
2026-02-20 00:23:12 +0100

    feat(message): add a translation toggle

--- Client/src/app/code-note/message/message.component.ts
+++ Client/src/app/code-note/message/message.component.ts
@@ -2,12 +2,16 @@ import {Component, input, output} from '@angular/core';
 import {DecodePipe} from "../decode.pipe";
 import {Letter} from "../letter/Letter";
 import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzSwitchComponent} from "ng-zorro-antd/switch";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-message',
   imports: [
     DecodePipe,
-    NzButtonComponent
+    NzButtonComponent,
+    NzSwitchComponent,
+    FormsModule
   ],
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
@@ -18,6 +22,12 @@ export class MessageComponent {
 
   delete = output<void>();
   open = output<void>();
+
+  isDecoded: boolean = true;
+
+  protected toggleDecode(value: boolean) {
+    this.isDecoded = value;
+  }
 
   protected deleteMessage() {
     this.delete.emit();
